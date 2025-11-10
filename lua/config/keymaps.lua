9bad6e223b098d1a7d1d5fa3a7dddd6a45b16db9
bd53 <lj6976294@mail.com>
2025-11-10 13:52:59 -0500

    adjust binds + gruvbox settings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,19 +1,18 @@
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>")
-vim.keymap.set("n", "<C-S-f>", ":Telescope live_grep<CR>")
-vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>")
+vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>", opts)
+vim.keymap.set("n", "<C-S-f>", ":Telescope live_grep<CR>", opts)
+vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", opts)
 
 vim.keymap.set("n", "<C-s>", ":w<CR>", opts)
 vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>a", opts)
+
 vim.keymap.set("n", "<C-z>", "u", opts)
 vim.keymap.set("i", "<C-z>", "<C-o>u", opts)
+
 vim.keymap.set("n", "<C-B>", function()
-  if vim.bo.filetype == "netrw" then
-    vim.cmd("bd")
-  else
-    vim.cmd("Ex")
-  end
+  if vim.bo.filetype == "netrw" then return vim.cmd("bd") end
+  vim.cmd("Ex")
 end, opts)
 
 vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
@@ -24,8 +23,7 @@ vim.keymap.set("n", "<leader>ct", ":bd!<CR>", opts)
 
 vim.keymap.set("n", "<leader>cd", function()
   vim.ui.input({ prompt = "Change directory: " }, function(input)
-    if input then
-      vim.cmd(("cd %s"):format(input))
-    end
+    if not input then return end
+    vim.cmd(("cd %s"):format(input))
   end)
-end)
+end, opts)
--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,13 +1,24 @@
 return {
   "ellisonleao/gruvbox.nvim",
   config = function()
+    require("gruvbox").setup({
+      undercurl = true,
+      underline = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      contrast = "hard",
+    })
+    vim.o.background = "dark"
+    vim.cmd.colorscheme("gruvbox")
+    vim.cmd("hi NonText guifg=bg")
     vim.keymap.set("n", "<leader>cb", function()
       vim.o.background = vim.o.background == "light" and "dark" or "light"
       vim.cmd("hi NonText guifg=bg")
     end)
-    require("gruvbox").setup({ contrast = "hard" })
-    vim.o.background = "dark"
-    vim.cmd.colorscheme("gruvbox")
-    vim.cmd("hi NonText guifg=bg")
   end,
 }
