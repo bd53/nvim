bd256b24e44d53dc1990eec8e2f3a1d4490045db
bd53 <lj6976294@mail.com>
2025-11-11 18:23:34 -0500

    cool stuff
    https://r2.fivemanage.com/3i2fhQIkHIaRFDy1YIvi8/Screenshotfrom2025-11-1118-17-53.png

--- lua/custom/comments.lua
+++ lua/custom/comments.lua
@@ -1,29 +1,51 @@
 local Comments = {}
 
 local Config = {
-  keywords = { "@todo", "@fix", "@ignore" },
-  comment_string = "//",
+  keywords = {
+    { word = "@todo", color = "#8b963b" },
+    { word = "@fix", color = "#2e7da1" },
+    { word = "@ignore", color = "#b63f3b" },
+  },
+  comment_strings = {
+    rs = "//",
+    cpp = "//",
+    ts = "//",
+    js = "//",
+    lua = "--",
+  },
 }
 
 local function setup_highlights()
-  vim.cmd("highlight CommentKeyword guifg=#5fad48 gui=bold")
-  for _, keyword in pairs(Config.keywords) do
-    vim.fn.matchadd("CommentKeyword", keyword)
+  for _, kw in pairs(Config.keywords) do
+    local group_name = "CommentKeyword" .. kw.word:gsub("@", "")
+    vim.cmd(string.format("highlight %s guifg=%s gui=bold", group_name, kw.color))
+  end
+end
+
+local function highlight_keywords()
+  local ft = vim.bo.filetype
+  local comment_str = Config.comment_strings[ft] or "//"
+  for _, kw in pairs(Config.keywords) do
+    local hl_group = "CommentKeyword" .. kw.word:gsub("@", "")
+    local pattern = vim.pesc(comment_str) .. ".*" .. vim.pesc(kw.word)
+    vim.fn.matchadd(hl_group, pattern)
   end
 end
 
 function Comments.toggle()
+  local ft = vim.bo.filetype
+  local comment_str = Config.comment_strings[ft] or "//"
   local line = vim.api.nvim_get_current_line()
-  local pattern = "^%s*" .. vim.pesc(Config.comment_string)
+  local pattern = "^%s*" .. vim.pesc(comment_str)
   if line:match(pattern) then
     line = line:gsub(pattern .. "%s?", "")
     vim.api.nvim_set_current_line(line)
     return
   end
-  line = Config.comment_string .. " " .. line
-  vim.api.nvim_set_current_line(line)
+  vim.api.nvim_set_current_line(comment_str .. " " .. line)
 end
 
 setup_highlights()
+highlight_keywords()
 
 return Comments
