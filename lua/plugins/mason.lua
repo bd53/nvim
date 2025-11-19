a2d3afe3b3c1e118d5b38f10f9976ccc20a5ead4
bd53 <lj6976294@mail.com>
2025-11-19 01:24:59 -0500

    remove inlay hints

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -10,7 +10,7 @@ return {
         dependencies = { "williamboman/mason.nvim" },
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "clangd", "lua_ls", "rust_analyzer", "ts_ls", "svelte" },
+                ensure_installed = { "clangd", "csharp_ls", "lua_ls", "rust_analyzer", "ts_ls", "svelte" },
                 automatic_installation = true,
             })
         end
@@ -23,6 +23,10 @@ return {
                 root_markers = { "CMakeLists.txt", "Makefile", "justfile", ".git" },
                 filetypes = { "c", "cpp" },
             })
+            vim.lsp.config("csharp_ls", {
+                root_markers = { "*.csproj", "*.sln", ".git" },
+                filetypes = { "cs" },
+            })
             vim.lsp.config("lua_ls", {
                 root_markers = { ".luarc.json", ".git" },
                 filetypes = { "lua" },
@@ -34,41 +38,23 @@ return {
                         diagnostics = {
                             globals = { "vim" },
                         },
-                        workspace = {
-                            library = vim.api.nvim_get_runtime_file("", true),
-                            checkThirdParty = false,
-                        },
-                        telemetry = { enable = false },
                     },
                 },
             })
             vim.lsp.config("rust_analyzer", {
                 root_dir = { "Cargo.toml", "Makefile", "justfile", ".git" },
                 filetypes = { "rust" },
-                settings = {
-                    ["rust-analyzer"] = {
-                        inlayHints = {
-                            enable = true,
-                            bindingModeHints = { enable = true },
-                            chainingHints = { enable = true },
-                            closingBraceHints = { enable = true, minLines = 10 },
-                            closureReturnTypeHints = { enable = "always" },
-                            lifetimeElisionHints = { enable = "always", useParameterNames = true },
-                            parameterHints = { enable = true },
-                            typeHints = { enable = true },
-                        },
-                    },
-                },
             })
             vim.lsp.config("ts_ls", {
                 root_markers = { "package.json", ".git" },
-                filetypes = { "typescript" },
+                filetypes = { "typescript", "javascript" },
             })
             vim.lsp.config("svelte", {
                 root_markers = { "svelte.config.js", "package.json", ".git" },
                 filetypes = { "svelte" },
             })
             vim.lsp.enable("clangd")
+            vim.lsp.enable("csharp_ls")
             vim.lsp.enable("lua_ls")
             vim.lsp.enable("rust_analyzer")
             vim.lsp.enable("ts_ls")
