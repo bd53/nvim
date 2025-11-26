d9df168f790e0d8bbe4b6a3b7e49fc31e383bcb1
bd53 <lj6976294@mail.com>
2025-11-26 05:38:25 -0500

    add binds from my personal editor
    Unfortunately neovim is still needed until I finish addressing a few
    remaining issues in my personal editor (https://github.com/bd53/ate).
    
    While it is fully functional and works as intended, there are still some
    small tweaks and adjustments that need to be made for it to be fully complete.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,11 @@
 local opts = { noremap = true, silent = true }
 
 vim.cmd("cnoreabbrev q q!")
+vim.keymap.set("n", "<c-q>", "<cmd>qa!<CR>", opts)
+vim.keymap.set({"i", "v"}, "<c-q>", "<Esc><cmd>qa!<CR>", opts)
 
 vim.keymap.set("n", "<c-a>", "ggVG", opts)
-vim.keymap.set("i", "<C-a>", "<Esc>ggVG", opts)
+vim.keymap.set("i", "<c-a>", "<Esc>ggVG", opts)
 
 vim.keymap.set({"n", "v"}, "<c-c>", '"+y', opts)
 
@@ -17,7 +19,7 @@ vim.keymap.set({"n", "i"}, "<c-z>", "<cmd>undo<CR>", opts)
 
 vim.keymap.set("n", "<leader>tc", function() require("custom.themes").toggle() end, opts)
 vim.keymap.set("n", "<leader>tp", function() require("custom.themes").picker() end, opts)
-vim.keymap.set("n", "<c-f>", function() require("custom.finder").toggle() end, opts)
+vim.keymap.set("n", "<c-p>", function() require("custom.finder").toggle() end, opts)
 
 vim.keymap.set("n", "<leader>gb", function() require("custom.git").blame() end, opts)
 vim.keymap.set("n", "<leader>gc", function() require("custom.git").changes() end, opts)
@@ -29,3 +31,15 @@ vim.keymap.set("n", "<leader>cd", function()
         vim.cmd.cd(input)
     end)
 end, opts)
+
+vim.keymap.set({"n", "i"}, "<c-o>", function()
+    vim.ui.input({ prompt = "Open file: " }, function(input)
+        if not input then return end
+        vim.cmd.edit(input)
+    end)
+end, opts)
+
+for _, mode in ipairs({"n", "i", "v"}) do
+    vim.keymap.set(mode, "<c-up>", mode == "i" and "<c-o>5k" or "3k", opts)
+    vim.keymap.set(mode, "<c-down>", mode == "i" and "<c-o>5j" or "3j", opts)
+end
